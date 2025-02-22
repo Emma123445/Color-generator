e2d58056ba16def8814ad5e27a56c74f46c62e6c
emma <emmanuellasodabi30@gmail.com>
2025-02-22 10:55:06 +0100

    mon fichier

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+const colorDisplay = document.createElement('h2');
+document.body.appendChild(colorDisplay);
 
 function getRandomRGBColor() {
-    const r = Math.floor(Math.random() * 256); // Valeur entre 0 et 255
-    const g = Math.floor(Math.random() * 256); // Valeur entre 0 et 255
-    const b = Math.floor(Math.random() * 256); // Valeur entre 0 et 255
-    return `rgb(${r}, ${g}, ${b})`; 
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
 }
+
+function changeBackgroundColor() {
+    const color = getRandomRGBColor();
+    document.body.style.backgroundColor = color;
+    colorDisplay.textContent = color;
+}
+
+setInterval(changeBackgroundColor, 1000);
